05747dbd74c08a1b63ab06545bac69a4a4f908e1
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-02-26 19:44:37 +0530

    Added review form

--- models/review.js
+++ models/review.js
@@ -2,8 +2,8 @@ const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
 const reviewSchema=new Schema({
-    body:String,
-    rating:Number
+    body:String,                //"Thsi is a great place"
+    rating:Number               //5
 });
 
 module.exports=mongoose.model("Review",reviewSchema);
