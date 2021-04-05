3a64c405c3e5720bf33700697205f5cbc386bc67
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-04-06 02:40:23 +0530

    Register route logic

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,24 @@
 const express= require('express');
 const router=express.Router();
+const catchAsync=require("../utils/catchAsync")
 const User=require("../models/user");
 
 router.get("/register",(req,res)=>{
     res.render("users/register");
 });
-router.post("/register",async(req,res)=>{
-    res.send(req.body)
-});
+router.post("/register",catchAsync(async(req,res)=>{
+    // res.send(req.body);
+    try{
+    const {email,username,password}=req.body;
+    const user=new User({email,username});
+    const registeredUser=await User.register(user,password);
+    req.flash("success","Welcome to picnicly");
+    res.redirect("/picnic_ground");
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("register");
+    }
+    // console.log(registeredUser);  
+}));
 
 module.exports=router;
