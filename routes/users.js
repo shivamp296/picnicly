7d7f5ede8ec6f85b720035467fb546d4f1ed6843
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-04-06 23:23:59 +0530

    Fixing register route

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,21 @@ const passport=require("passport");
 router.get("/register",(req,res)=>{
     res.render("users/register");
 });
-router.post("/register",catchAsync(async(req,res)=>{
+router.post("/register",catchAsync(async(req,res,next)=>{
     // res.send(req.body);
     try{
     const {email,username,password}=req.body;
     const user=new User({email,username});
     const registeredUser=await User.register(user,password);
-    req.flash("success","Welcome to picnicly");
-    res.redirect("/picnic_ground");
+    //after registeration we want the user should be automatcally logged in... no need to login once again
+    req.login(registeredUser,err=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","Welcome to picnicly");
+        res.redirect("/picnic_ground");
+    })
+    
     }catch(e){
         req.flash("error",e.message);
         res.redirect("register");
