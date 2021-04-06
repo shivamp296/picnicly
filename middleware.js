37f2a433003d4316ffa0a51cf8cd3231dd17996d
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-04-06 23:17:04 +0530

    Added currentUser helper

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn=(req,res,next)=>{
+    // console.log("req.user...",req.user); give details of current user
     if(!req.isAuthenticated()){
         req.flash("error","You must be signed in first");
         return res.redirect("/login");
