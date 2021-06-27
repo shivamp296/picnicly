c545b8e1d20082ecfc0f87782349070b7cd119dd
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-06-28 04:08:06 +0530

    Centering the map on our picnic ground

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -4,6 +4,12 @@
   const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  center: picnic_info.geometry.coordinates, // starting position [lng, lat]
+  zoom: 7 // starting zoom
   });
+
+  //setting marker
+  new mapboxgl.Marker()
+  .setLngLat(picnic_info.geometry.coordinates)
+  .addTo(map)
+
