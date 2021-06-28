ffb44e200e9d75fa0b70a538dc92c41127a13f05
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-06-28 14:06:59 +0530

    Customising map popup

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -4,6 +4,7 @@
   const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  // style: 'mapbox://styles/mapbox/light-v10',  //light color
   center: picnic_info.geometry.coordinates, // starting position [lng, lat]
   zoom: 7 // starting zoom
   });
@@ -11,5 +12,11 @@
   //setting marker
   new mapboxgl.Marker()
   .setLngLat(picnic_info.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({offset:25})
+      .setHTML(
+        `<h3>${picnic_info.title}</h3><p>${picnic_info.location}</p>`
+      )
+  )
   .addTo(map)
 
