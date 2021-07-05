3117b07fccdd89715b850f120cf80e44a1cdfcee
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-07-05 23:31:16 +0530

    Setting up port for heroku once again

--- app.js
+++ app.js
@@ -259,5 +259,5 @@ app.use((err,req,res,next)=>{
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
-    console.log("Listening on the port : 3000");
+    console.log(`Listening on the port : ${port}`);
 });
